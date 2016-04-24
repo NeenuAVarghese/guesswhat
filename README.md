56edc818fbedc79a48543ba65b0e34d98e59c3ae
mittman <mittman@users.noreply.github.com>
2016-04-24 05:36:21 -0700

    Improved Redis support

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # guesswhat
 
+###Prerequisites
+* [nodeJS](https://nodejs.org)
+* [Redis](http://redis.io/download) (NoSQL Database)
+
 ###Installation
 1. `git clone https://github.com/NeenuAVarghese/guesswhat.git`
 2. `cd guesswhat`
 3. `npm install`
 4. `npm start`
-5. Open web browser to `http://localhost:8080`
+5. Open web browser to `http://localhost:3000`
 6. Repeat Step 5, for each client
 
 ###Testing
--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@
 "use strict";
 
 // Config
-var port = 3000;
+var httpPort = 3000;
+var redisPort = 7777;
 var line_history = [];
 var usernames = {};
 var magic = [ "bunny", "dog", "elephant", "fish", "turtle" ];
@@ -16,23 +17,40 @@ var io = require("socket.io");
 var redis = require("redis");
 
 // Initialize
-var redisClient = redis.createClient();
+
 var app = express();
 app.use(express.static("./"));
 var server = null;
+var redisClient = null;
+var db = false;
 
 // Functions
+function connectDB() {
+    redisClient = redis.createClient(redisPort);
+    redisClient.on("error", function() {
+        console.error("Redis server refused connection on port", redisPort);
+        return;
+    });
+
+    redisClient.on("connect", function() {
+        console.log("Connected to Redis Server on port", redisPort);
+        db = true;
+    });
+
+    redisClient.on("end", function() {
+        console.log("Connection to Redis Server closed");
+        db = false;
+    });
+}
+
 function startServer() {
-    var process = io.listen(app.listen(port).on("error", function(err) {
+    var process = io.listen(app.listen(httpPort).on("error", function(err) {
         if (!err) {
-            console.log("Starting express server on port", port);
-            redisClient.on("connect", function() {
-                console.log("Connected to Redis Server");
-            });
+            console.log("Starting express server on httpPort", httpPort);
         }
         else if (err.errno === "EADDRINUSE") {
-            console.log("Port", port, "busy. Unable to start express server");
-            console.log("To debug: $ lsof -i :" + port);
+            console.log("httpPort", httpPort, "busy. Unable to start express server");
+            console.log("To debug: $ lsof -i :" + httpPort);
         }
         else {
             console.log(err);
@@ -49,15 +67,20 @@ function userLogin(socket) {
         socket.username = username;
 
         // store username in Redis database
-        redisClient.exists(username, function(err, object) {
-            if (object !== 1) {
-                // initialize score to 0
-                redisClient.hmset(username, {
-                    "score": 0
-                });
-                redisClient.rpush("users", username);
-            }
-        });
+        if (db) {
+            redisClient.exists(username, function(err, object) {
+                if (object !== 1) {
+                    // initialize scores to 0
+                    redisClient.hmset(username, {
+                        "wins": 0,
+                    });
+                    redisClient.hmset(username, {
+                        "losses": 0,
+                    });
+                    redisClient.rpush("users", username);
+                }
+            });
+        }
 
         // echo to client they've connected
         socket.emit("updatechat", "SERVER", "you have connected");
@@ -65,15 +88,20 @@ function userLogin(socket) {
         socket.broadcast.emit("updatechat", "SERVER", username + " has connected");
 
         // add the client's username to the global list
-        redisClient.lrange("users", 0, -1, function(err, items) {
-            if (err) {
-                console.log("Error in getting elements of user list");
-            }
-            items.forEach(function(item) {
-                console.log("Item", item);
-                usernames[username] = username;
+        if (db) {
+            redisClient.lrange("users", 0, -1, function(err, items) {
+                if (err) {
+                    console.log("Error in getting elements of user list");
+                }
+                items.forEach(function(item) {
+                    console.log("Item", item);
+                    usernames[username] = username;
+                });
             });
-        });
+        }
+        else {
+            usernames[username] = username;
+        }
 
         // update the list of users in chat, client-side
         server.sockets.emit("updateusers", usernames);
@@ -86,11 +114,13 @@ function userLogout(socket) {
         console.log("User:", socket.username, "Disconnected");
 
         // remove the username from global usernames list
-        redisClient.lrem("users",1,  socket.username, function(err) {
-            if (err) {
-                console.log("User Removed form list");
-            }
-        });
+        if (db) {
+            redisClient.lrem("users",1,  socket.username, function(err) {
+                if (err) {
+                    console.log("User Removed form list");
+                }
+            });
+        }
 
         delete usernames[socket.username];
         // update list of users in chat, client-side
@@ -118,6 +148,24 @@ function transmitDraw(socket) {
     });
 }
 
+function winner(socket) {
+    console.log("Winner", socket.username);
+    // echo to client they've won
+    server.sockets.emit("updateword", "you win!");
+    server.sockets.emit("updateword", "the word was '" + magic[xyzzy] + "'");
+    // echo globally (all clients) that a person has won
+    socket.broadcast.emit("updateword", "the word was '" + magic[xyzzy] + "'");
+    socket.broadcast.emit("updateword", "player " + socket.username + " was the winner");
+
+    // change word
+    if (xyzzy < magic.length) {
+        xyzzy += 1;
+    }
+    else {
+        xyzzy = 0;
+    }
+}
+
 function parseChat(socket, data) {
     var line = Array.prototype.join.call(data, "");
     var words = line.split(" ");
@@ -125,21 +173,7 @@ function parseChat(socket, data) {
     for (var i = 0; i < words.length; ++i) {
         console.log(words[i], magic[xyzzy]);
         if (words[i] === magic[xyzzy]) {
-            console.log("Winner", socket.username);
-            // echo to client they've won
-            server.sockets.emit("updateword", "you win!");
-            server.sockets.emit("updateword", "the word was '" + magic[xyzzy] + "'");
-            // echo globally (all clients) that a person has won
-            socket.broadcast.emit("updateword", "the word was '" + magic[xyzzy] + "'");
-            socket.broadcast.emit("updateword", "player " + socket.username + " was the winner");
-
-            // change word
-            if (xyzzy < magic.length) {
-                xyzzy += 1;
-            }
-            else {
-                xyzzy = 0;
-            }
+            winner(socket, magic[xyzzy]);
         }
     }
 }
@@ -157,6 +191,7 @@ function transmitChat(socket) {
 
 // Run server
 server = startServer();
+connectDB();
 
 // Main
 server.sockets.on("connection", function(socket) {

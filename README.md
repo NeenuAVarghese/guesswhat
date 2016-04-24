e37fa2f8c49591c21c029f59810203f063791c8e
mittman <mittman@users.noreply.github.com>
2016-04-23 21:15:15 -0700

    Added validation script. Run 'npm test'

--- README.md
+++ README.md
@@ -8,4 +8,8 @@
 5. Open web browser to `http://localhost:8080`
 6. Repeat Step 5, for each client
 
-
+###Testing
+1. Install [csslint](https://www.npmjs.com/package/csslint) (CSS validator)
+2. Install [jshint](https://www.npmjs.com/package/jshint) (Javascript validator)
+3. Install [tidy](https://github.com/htacg/tidy-html5/tree/master/README) (HTML validator)
+4. `npm test`
---
+++ scripts/validate.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+##################
+# validate.sh #######
+version="1.0" #######
+### Kevin Mittman ###
+#####################
+
+html=("index.html")
+css=("style.css")
+js=("app.js" "client.js")
+
+checkdep() { type -p $1 &>/dev/null; }
+
+alert="\e[1;34m"
+notice="\e[1;39m"
+warn="\e[1;33m"
+fail="\e[1;31m"
+reset="\e[0m"
+
+run_utility() {
+  if checkdep $cmd; then
+    echo -e "${alert}==>${reset} ${notice}Running '${cmd}' ${what} validation${reset}"
+    for file in ${input[@]}; do
+      if [ -f "$file" ]; then
+        output=$($cmd $params "$file" 2>&1)
+        [ ! -z "$output" ] && error="true" && echo -e "${fail}==> FAIL${reset} $file" && echo "$output"
+      else
+        echo -e "${warn}==> WARN${reset} Not a file: $file"
+      fi
+    done
+  else
+    echo -e "${warn}Missing ${cmd}${reset}. Skipping ${what} validation"
+  fi
+}
+
+whitespace() {
+  if checkdep grep; then
+    if [ -f "$1" ]; then
+      GREP_COLOR="1;30;41" grep -n --color=always " $" "$1"
+    else
+      echo -e "${warn}==> WARN${reset} Not a file: $1"
+    fi
+  else
+    echo -e "${warn}Missing grep${reset}. Skipping ${what} validation"
+  fi
+}
+
+parse_utils() {
+  for cmd in $list; do
+    if [ $cmd = "tidy" ]; then
+      what="HTML"
+      params="-qe"
+      input="$html"
+      run_utility
+    elif [ "$cmd" = "csslint" ]; then
+      what="CSS"
+      params="--quiet"
+      input="$css"
+      run_utility
+    elif [ "$cmd" = "jshint" ]; then
+      what="Javascript"
+      params=""
+      input="$js"
+      run_utility
+    elif [ "$cmd" = "whitespace" ]; then
+      what="Whitespace"
+      params=""
+      input=("${html[@]}" "${css[@]}" "${js[@]}")
+      run_utility
+    else
+      echo -e "${fail}==> FAIL${reset} Unknown command $cmd"
+    fi
+  done
+
+  #[ "$error" = "true" ] && exit 1
+}
+
+if [ -f ".webapp" ]; then
+  echo -e "${alert}==>${reset} ${notice}Parsing ($PWD/.webapp) config file${reset}"
+  source ".webapp"
+else
+  echo -e "${warn}==>${reset} ${notice}Config file ($PWD/.webapp) not found${reset}"
+fi
+
+list="jshint csslint tidy whitespace"
+parse_utils
+
+### END ###

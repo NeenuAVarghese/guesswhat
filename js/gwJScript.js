14575538df75dc6046f98aafaed1fe01fe9900c2
mittman <mittman@users.noreply.github.com>
2016-05-16 09:17:18 -0700

    Quick fix for issue #27

--- js/gwJScript.js
+++ js/gwJScript.js
@@ -458,6 +458,7 @@ var main = function() {
         console.log(data.magicwrdmeaning, data.magicwrd);
         $(gw.landpage.section.content.hintCard.definition).text(data.magicwrdmeaning);
         $(gw.landpage.section.content.hintCard.word).text(data.magicwrd);
+        $(gw.landpage.section.content.hintCard.handle).modal({backdrop: "static",keyboard: false});
         $(gw.landpage.section.content.hintCard.handle).modal("show");
         $(gw.landpage.section.navbar.startGame).hide();
         console.log("painting", painting);
